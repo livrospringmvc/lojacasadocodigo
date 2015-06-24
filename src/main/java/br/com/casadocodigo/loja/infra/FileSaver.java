2ba79bd6b917bd44d6d7dd16727e8546fc77bfdb
asouza <alots.ssa@gmail.com>
2015-06-24 11:48:01 -0300

    [upload] usando o s3ninja para simular

--- src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
+++ src/main/java/br/com/casadocodigo/loja/infra/FileSaver.java
@@ -1,6 +1,5 @@
 package br.com.casadocodigo.loja.infra;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+
 @Component
 public class FileSaver {
 
@@ -16,16 +23,28 @@ public class FileSaver {
 	private HttpServletRequest request;
 
 	public String write(String baseFolder, MultipartFile multipartFile) {
-		String realPath = request.getServletContext().getRealPath("/"+baseFolder);
+		AmazonS3Client s3 = client();
 		try {
-			String path = realPath+"/"+multipartFile.getOriginalFilename();
-			multipartFile.transferTo(new File(path));
-			return baseFolder+"/"+multipartFile.getOriginalFilename();
-		} catch (IOException e) {
+			s3.putObject("casadocodigo", multipartFile.getOriginalFilename(),
+					multipartFile.getInputStream(), new ObjectMetadata());
+			return "http://localhost:9444/s3/casadocodigo/"+multipartFile.getOriginalFilename();
+		} catch (AmazonClientException | IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
-	
+
+	private AmazonS3Client client() {
+		AWSCredentials credentials = new BasicAWSCredentials(
+				"AKIAIOSFODNN7EXAMPLE",
+				"wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
+		AmazonS3Client newClient = new AmazonS3Client(credentials,
+				new ClientConfiguration());
+		newClient.setS3ClientOptions(new S3ClientOptions()
+				.withPathStyleAccess(true));
+		newClient.setEndpoint("http://localhost:9444/s3");
+		return newClient;
+
+	}
+
 }
---
+++ src/test/java/br/com/casadocodigo/loja/infra/FileSaverTest.java
@@ -0,0 +1,27 @@
+package br.com.casadocodigo.loja.infra;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.S3ClientOptions;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+public class FileSaverTest {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		AWSCredentials credentials = new BasicAWSCredentials("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY");
+        AmazonS3Client newClient = new AmazonS3Client(credentials,
+                new ClientConfiguration());
+        newClient.setS3ClientOptions(new S3ClientOptions().withPathStyleAccess(true));
+        newClient.setEndpoint("http://localhost:9444/s3");
+        FileInputStream file = new FileInputStream(new File("/Users/alberto/Desktop/s3ninja-sempasta.png"));
+        PutObjectResult putObject = newClient.putObject("casadocodigo", "arquivo.java", file, new ObjectMetadata());
+		
+	}
+}
